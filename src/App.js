86720b52de73bfdce046f03fcb433718dc6a17b7
Ilyas <ilyas.bakhytzhan@gmail.com>
2025-08-04 16:29:54 +0500

    Bg fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import react, {useState} from 'react';
-import WeatherCard from "./WeatherCard";
-import './App.css';
-
+import { useState } from 'react'
+import './App.css'
+import WeatherCard from './WeatherCard'
 
 const App = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('')
+  const [weatherData, setWeatherData] = useState(null)
 
-  const apiKey = "cabaf4e530574a1deda59c64ba255aa1";
-  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+  const apiKey = 'cabaf4e530574a1deda59c64ba255aa1'
+  const apiUrl =
+    'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
-  const checkWeather = async (city) => {
+  const checkWeather = async city => {
     try {
-      const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-      const data = await response.json();
+      const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+      const data = await response.json()
       if (response.ok) {
-        setWeatherData(data);
+        setWeatherData(data)
       } else {
-        alert("City not found!");
+        alert('City not found!')
       }
     } catch (error) {
-      alert("Error fetching weather data. Please try again.");
+      alert('Error fetching weather data. Please try again.')
     }
-  };
+  }
 
   return (
-    <div className="app">
+    <div className='app'>
       <WeatherCard checkWeather={checkWeather} weatherData={weatherData} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
